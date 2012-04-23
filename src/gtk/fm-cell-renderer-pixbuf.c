246585611ebc30632b01ddbf5848e2eba04f0f5e
Axel FILMORE <axel.filmore@gmail.com>
2012-04-23 07:45:24 +0200

    Initial Support For Gtk3...

--- src/gtk/fm-cell-renderer-pixbuf.c
+++ src/gtk/fm-cell-renderer-pixbuf.c
@@ -6,7 +6,7 @@
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
  *      the Free Software Foundation; either version 2 of the License, or
- *      (at your option) any later version.
+ *       (at your option) any later version.
  *      
  *      This program is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,32 +21,33 @@
 
 #include "fm-cell-renderer-pixbuf.h"
 
-static void fm_cell_renderer_pixbuf_finalize  			(GObject *object);
+static void fm_cell_renderer_pixbuf_finalize  			 (GObject *object);
 
-static void fm_cell_renderer_pixbuf_get_size   (GtkCellRenderer            *cell,
+static void fm_cell_renderer_pixbuf_get_size    (GtkCellRenderer            *cell,
 						 GtkWidget                  *widget,
 						 GdkRectangle               *rectangle,
 						 gint                       *x_offset,
 						 gint                       *y_offset,
 						 gint                       *width,
 						 gint                       *height);
-static void fm_cell_renderer_pixbuf_render     (GtkCellRenderer            *cell,
-						 GdkDrawable                *window,
+
+static void fm_cell_renderer_pixbuf_render      (GtkCellRenderer            *cell,
+						 GdkWindow                *window,
 						 GtkWidget                  *widget,
 						 GdkRectangle               *background_area,
 						 GdkRectangle               *cell_area,
 						 GdkRectangle               *expose_area,
 						 GtkCellRendererState        flags);
 
-static void fm_cell_renderer_pixbuf_get_property ( GObject *object,
+static void fm_cell_renderer_pixbuf_get_property (GObject *object,
                                       guint param_id,
                                       GValue *value,
-                                      GParamSpec *pspec );
+                                      GParamSpec *pspec);
 
-static void fm_cell_renderer_pixbuf_set_property ( GObject *object,
+static void fm_cell_renderer_pixbuf_set_property (GObject *object,
                                       guint param_id,
                                       const GValue *value,
-                                      GParamSpec *pspec );
+                                      GParamSpec *pspec);
 
 enum
 {
@@ -54,32 +55,32 @@ enum
     N_PROPS
 };
 
-G_DEFINE_TYPE(FmCellRendererPixbuf, fm_cell_renderer_pixbuf, GTK_TYPE_CELL_RENDERER_PIXBUF);
+G_DEFINE_TYPE (FmCellRendererPixbuf, fm_cell_renderer_pixbuf, GTK_TYPE_CELL_RENDERER_PIXBUF);
 
 static GdkPixbuf* link_icon = NULL;
 
 /* GdkPixbuf RGB C-Source image dump 1-byte-run-length-encoded */
 
 #ifdef __SUNPRO_C
-#pragma align 4 (link_icon_data)
+#pragma align 4  (link_icon_data)
 #endif
 #ifdef __GNUC__
-static const guint8 link_icon_data[] __attribute__ ((__aligned__ (4))) = 
+static const guint8 link_icon_data[] __attribute__  ((__aligned__  (4))) = 
 #else
 static const guint8 link_icon_data[] = 
 #endif
 { ""
-  /* Pixbuf magic (0x47646b50) */
+  /* Pixbuf magic  (0x47646b50) */
   "GdkP"
-  /* length: header (24) + pixel_data (148) */
+  /* length: header  (24) + pixel_data  (148) */
   "\0\0\0\254"
-  /* pixdata_type (0x2010001) */
+  /* pixdata_type  (0x2010001) */
   "\2\1\0\1"
-  /* rowstride (30) */
+  /* rowstride  (30) */
   "\0\0\0\36"
-  /* width (10) */
+  /* width  (10) */
   "\0\0\0\12"
-  /* height (10) */
+  /* height  (10) */
   "\0\0\0\12"
   /* pixel_data: */
   "\1\0\0\0\211\200\200\200\1\0\0\0\210\377\377\377\2\200\200\200\0\0\0"
@@ -92,10 +93,10 @@ static const guint8 link_icon_data[] =
 
 
 
-static void fm_cell_renderer_pixbuf_class_init(FmCellRendererPixbufClass *klass)
+static void fm_cell_renderer_pixbuf_class_init (FmCellRendererPixbufClass *klass)
 {
-	GObjectClass *g_object_class = G_OBJECT_CLASS(klass);
-    GtkCellRendererClass *cell_class = GTK_CELL_RENDERER_CLASS(klass);
+	GObjectClass *g_object_class = G_OBJECT_CLASS (klass);
+    GtkCellRendererClass *cell_class = GTK_CELL_RENDERER_CLASS (klass);
 
 	g_object_class->finalize = fm_cell_renderer_pixbuf_finalize;
     g_object_class->get_property = fm_cell_renderer_pixbuf_get_property;
@@ -105,93 +106,93 @@ static void fm_cell_renderer_pixbuf_class_init(FmCellRendererPixbufClass *klass)
     cell_class->render = fm_cell_renderer_pixbuf_render;
 
 
-    g_object_class_install_property ( g_object_class,
+    g_object_class_install_property (g_object_class,
                                       PROP_INFO,
-                                      g_param_spec_pointer ( "info",
+                                      g_param_spec_pointer ("info",
                                                              "File info",
                                                              "File info",
-                                                             G_PARAM_READWRITE ) );
+                                                             G_PARAM_READWRITE));
 }
 
 
-static void fm_cell_renderer_pixbuf_finalize(GObject *object)
+static void fm_cell_renderer_pixbuf_finalize (GObject *object)
 {
 	FmCellRendererPixbuf *self;
 
-	g_return_if_fail(object != NULL);
-	g_return_if_fail(FM_IS_CELL_RENDERER_PIXBUF(object));
+	g_return_if_fail (object != NULL);
+	g_return_if_fail (FM_IS_CELL_RENDERER_PIXBUF (object));
 
-	self = FM_CELL_RENDERER_PIXBUF(object);
-    if( self->fi )
-        fm_file_info_unref(self->fi);
+	self = FM_CELL_RENDERER_PIXBUF (object);
+    if (self->fi)
+        fm_file_info_unref (self->fi);
 
-	G_OBJECT_CLASS(fm_cell_renderer_pixbuf_parent_class)->finalize(object);
+	G_OBJECT_CLASS (fm_cell_renderer_pixbuf_parent_class)->finalize (object);
 }
 
 
-static void fm_cell_renderer_pixbuf_init(FmCellRendererPixbuf *self)
+static void fm_cell_renderer_pixbuf_init (FmCellRendererPixbuf *self)
 {
-    if( !link_icon )
+    if (!link_icon)
     {
-        link_icon = gdk_pixbuf_new_from_inline(
-                            sizeof(link_icon_data),
+        link_icon = gdk_pixbuf_new_from_inline (
+                            sizeof (link_icon_data),
                             link_icon_data,
-                            FALSE, NULL );
-        g_object_add_weak_pointer((GObject*)link_icon, (gpointer)&link_icon);
+                            FALSE, NULL);
+        g_object_add_weak_pointer ((GObject*)link_icon,  (gpointer)&link_icon);
     }
     else
-        g_object_ref(link_icon);
+        g_object_ref (link_icon);
 }
 
 
-GtkCellRenderer *fm_cell_renderer_pixbuf_new(void)
+GtkCellRenderer *fm_cell_renderer_pixbuf_new (void)
 {
-	return g_object_new(FM_TYPE_CELL_RENDERER_PIXBUF, NULL);
+	return g_object_new (FM_TYPE_CELL_RENDERER_PIXBUF, NULL);
 }
 
-static void fm_cell_renderer_pixbuf_get_property ( GObject *object,
+static void fm_cell_renderer_pixbuf_get_property (GObject *object,
                                       guint param_id,
                                       GValue *value,
-                                      GParamSpec *psec )
+                                      GParamSpec *psec)
 {
-    FmCellRendererPixbuf* renderer = (FmCellRendererPixbuf*)object;
-    switch( param_id )
+    FmCellRendererPixbuf* renderer =  (FmCellRendererPixbuf*)object;
+    switch (param_id)
     {
     case PROP_INFO:
-        g_value_set_pointer(value, renderer->fi ? fm_file_info_ref(renderer->fi) : NULL);
+        g_value_set_pointer (value, renderer->fi ? fm_file_info_ref (renderer->fi) : NULL);
         break;
     default:
-        G_OBJECT_WARN_INVALID_PROPERTY_ID ( object, param_id, psec );
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, psec);
         break;
     }
 }
 
-static void fm_cell_renderer_pixbuf_set_property ( GObject *object,
+static void fm_cell_renderer_pixbuf_set_property (GObject *object,
                                       guint param_id,
                                       const GValue *value,
-                                      GParamSpec *psec )
+                                      GParamSpec *psec)
 {
-    FmCellRendererPixbuf* renderer = (FmCellRendererPixbuf*)object;
-    switch ( param_id )
+    FmCellRendererPixbuf* renderer =  (FmCellRendererPixbuf*)object;
+    switch (param_id)
     {
     case PROP_INFO:
-        if( renderer->fi )
-            fm_file_info_unref(renderer->fi);
-        renderer->fi = fm_file_info_ref((FmFileInfo*)g_value_get_pointer(value));
+        if (renderer->fi)
+            fm_file_info_unref (renderer->fi);
+        renderer->fi = fm_file_info_ref ((FmFileInfo*)g_value_get_pointer (value));
         break;
     default:
-        G_OBJECT_WARN_INVALID_PROPERTY_ID ( object, param_id, psec );
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, psec);
         break;
     }
 }
 
-void fm_cell_renderer_pixbuf_set_fixed_size(FmCellRendererPixbuf* render, gint w, gint h)
+void fm_cell_renderer_pixbuf_set_fixed_size (FmCellRendererPixbuf* render, gint w, gint h)
 {
     render->fixed_w = w;
     render->fixed_h = h;
 }
 
-void fm_cell_renderer_pixbuf_get_size   (GtkCellRenderer            *cell,
+void fm_cell_renderer_pixbuf_get_size    (GtkCellRenderer            *cell,
 						 GtkWidget                  *widget,
 						 GdkRectangle               *rectangle,
 						 gint                       *x_offset,
@@ -199,48 +200,96 @@ void fm_cell_renderer_pixbuf_get_size   (GtkCellRenderer            *cell,
 						 gint                       *width,
 						 gint                       *height)
 {
-    FmCellRendererPixbuf* render = (FmCellRendererPixbuf*)cell;
-    if(render->fixed_w > 0 && render->fixed_h > 0)
+    FmCellRendererPixbuf* render =  (FmCellRendererPixbuf*)cell;
+    if (render->fixed_w > 0 && render->fixed_h > 0)
     {
         *width = render->fixed_w;
         *height = render->fixed_h;
     }
     else
     {
-        GTK_CELL_RENDERER_CLASS(fm_cell_renderer_pixbuf_parent_class)->get_size(cell, widget, rectangle, x_offset, y_offset, width, height);
+        GTK_CELL_RENDERER_CLASS (fm_cell_renderer_pixbuf_parent_class)->get_size (cell, widget, rectangle, x_offset, y_offset, width, height);
     }
 }
 
-void fm_cell_renderer_pixbuf_render     (GtkCellRenderer            *cell,
-						 GdkDrawable                *window,
+void fm_cell_renderer_pixbuf_render      (GtkCellRenderer            *cell,
+						 GdkWindow                *window,
 						 GtkWidget                  *widget,
 						 GdkRectangle               *background_area,
 						 GdkRectangle               *cell_area,
 						 GdkRectangle               *expose_area,
 						 GtkCellRendererState        flags)
 {
-    FmCellRendererPixbuf* render = (FmCellRendererPixbuf*)cell;
+    FmCellRendererPixbuf* render =  (FmCellRendererPixbuf*)cell;
     /* we don't need to follow state for prelit items */
-    if(flags & GTK_CELL_RENDERER_PRELIT)
+    if (flags & GTK_CELL_RENDERER_PRELIT)
         flags &= ~GTK_CELL_RENDERER_PRELIT;
-    GTK_CELL_RENDERER_CLASS(fm_cell_renderer_pixbuf_parent_class)->render(cell, window, widget, background_area, cell_area, expose_area, flags);
+    
+    /***
+     * In Gtk3 the GdkWindow is replaced by a Cairo context...
+     * 
+     * 
+        http://developer.gnome.org/gtk/2.24/GtkCellRenderer.html#gtk-cell-renderer-render
+*       http://developer.gnome.org/gtk3/stable/GtkCellRenderer.html#gtk-cell-renderer-render
 
-    if(render->fi && G_UNLIKELY(fm_file_info_is_symlink(render->fi)))
+    void                gtk_cell_renderer_render            (GtkCellRenderer *cell,
+                                                         GdkWindow *window,
+                                                         GtkWidget *widget,
+                                                         const GdkRectangle *background_area,
+                                                         const GdkRectangle *cell_area,
+                                                         const GdkRectangle *expose_area,
+                                                         GtkCellRendererState flags);
+    
+    
+    void                gtk_cell_renderer_render            (GtkCellRenderer *cell,
+                                                         cairo_t *cr,
+                                                         GtkWidget *widget,
+                                                         const GdkRectangle *background_area,
+                                                         const GdkRectangle *cell_area,
+                                                         GtkCellRendererState flags);
+    ***/
+
+
+
+    GTK_CELL_RENDERER_CLASS (fm_cell_renderer_pixbuf_parent_class)->render (cell,
+                                                                            window,
+                                                                            widget,
+                                                                            background_area,
+                                                                            cell_area,
+                                                                            expose_area,
+                                                                            flags);
+
+    if (render->fi && G_UNLIKELY (fm_file_info_is_symlink (render->fi)))
     {
         GdkRectangle pix_rect;
         GdkPixbuf* pix;
-        g_object_get(render, "pixbuf", &pix, NULL);
-        if(pix)
+        
+        g_object_get (render, "pixbuf", &pix, NULL);
+        
+        if (pix)
         {
-            //~ int x = cell_area->x + (cell_area->width - gdk_pixbuf_get_width(pix))/2;
-            //~ int y = cell_area->y + (cell_area->height - gdk_pixbuf_get_height(pix))/2;
+            //~ int x = cell_area->x +  (cell_area->width - gdk_pixbuf_get_width (pix))/2;
+            //~ int y = cell_area->y +  (cell_area->height - gdk_pixbuf_get_height (pix))/2;
             
             int x = cell_area->x;
-            int y = (cell_area->y + (cell_area->height - gdk_pixbuf_get_height(pix))/2) + gdk_pixbuf_get_height(pix) -10;
-
-            gdk_draw_pixbuf ( GDK_DRAWABLE ( window ), NULL, link_icon, 0, 0,
-                              x, y, -1, -1, GDK_RGB_DITHER_NORMAL, 0, 0 );
-            g_object_unref(pix);
+            int y =  (cell_area->y +  (cell_area->height - gdk_pixbuf_get_height (pix))/2) + gdk_pixbuf_get_height (pix) -10;
+            
+            pix_rect.x = x;
+            pix_rect.y = y;
+            pix_rect.width = gdk_pixbuf_get_width (pix);
+            pix_rect.height = gdk_pixbuf_get_height (pix);
+            
+            cairo_t *cr = gdk_cairo_create (window);
+            gdk_cairo_set_source_pixbuf (cr, link_icon, x, y);
+            gdk_cairo_rectangle (cr, &pix_rect);
+            cairo_fill (cr);
+            
+            cairo_destroy (cr);
+            
+            //~ gdk_draw_pixbuf (GDK_DRAWABLE (window), NULL, link_icon, 0, 0,
+                              //~ x, y, -1, -1, GDK_RGB_DITHER_NORMAL, 0, 0);
+            
+            g_object_unref (pix);
         }
     }
 }
